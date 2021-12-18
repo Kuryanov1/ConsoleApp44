fca2c4ed3f79cf4c48ebca0f653ff2fa7d0f7257
kurya <kurya@LAPTOP-HGLSULKT>
2021-12-18 17:44:27 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                string filePath = @"C:\rr\test.txt";
+                List<User> people = new List<User>();
+                List<string> lines = File.ReadAllLines(filePath).ToList();
+                foreach (var line in lines)
+                {
+                    
+                    string[] entries = line.Split(' ');
+                    User newUser = new User();
+                    newUser.LastName = entries[0];
+                    newUser.Name = entries[1];
+                    newUser.MiddleName = entries[2];
+                    newUser.Age = Convert.ToDateTime(entries[3]);
+                    db.Users.Add(newUser);
+                        
+                }
+          
+                db.SaveChanges();
+                
+
+
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+
+                    Console.WriteLine($" Фамилия: {u.LastName} | Имя: {u.Name} | Отчество: {u.MiddleName} |Возраст: {u.Age}"); //Вывод всех объектов
+
+                }
+                }
+                Console.Read();
+            }
+        }
+    }
---
+++ ConsoleApp4/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public DateTime Age { get; set; }
+    }
+}
